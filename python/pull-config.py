18e5e81487c764ed508789c7d2827f91fd09529b
dlakey <lakesystem@gmail.com>
2018-03-02 14:22:18 -0800

    Carnival Repo Updates

---
+++ python/pull-config.py
@@ -0,0 +1,42 @@
+import paramiko
+import time
+import base64
+import sys
+import cmd
+import json
+import yaml
+import os
+
+host = raw_input('Enter Hostname or IP address of Device: ')
+
+commands = ['show vlan']
+
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect(host, username='admin', password='cc2lake', look_for_keys=False, allow_agent=False)
+dev = ssh.invoke_shell()
+
+# Enable Mode
+#dev.send('enable\n')
+
+#Enable Password
+#dev.send('cc2lake\n')
+resp = dev.recv(0)
+
+#Turn of Terminal Paging
+dev.send('terminal length 0\n')
+resp = dev.recv(0)
+
+#Dump Config
+dev.send('show run\n')
+time.sleep(3)
+resp = dev.recv(49999)
+counter = 0
+print resp
+with open('../roles/' + host + '/files/' + host + '.cfg', 'w') as f:
+	for line in resp:
+		f.write(line)
+
+
+	#resp = dev.recv(9999)
+	#print resp
